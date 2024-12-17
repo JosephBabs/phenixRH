94717ca757db551f6e12a1cf2c673f2fd728c91a
JosephBabs <bajos3d@gmail.com>
2024-12-17 08:49:28 +0100

    main route to login

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PaySlipController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\PaiementController;
 
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
 // Authentication Routes
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
